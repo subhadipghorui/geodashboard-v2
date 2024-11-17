55463a47f8db7eca85a0fa5e37a2b8d8c2c5d368
Subhadip G <subhadipghorui105@gmail.com>
2024-11-17 12:59:38 +0530

    added api fgor map and layer

---
+++ geodashboard-app/app/Http/Controllers/Api/v1/MapController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Map;
+use Illuminate\Http\Request;
+
+class MapController extends Controller
+{
+    public function index(){
+        $data["map"] = Map::all();
+        return response()->json([
+            "error" => false,
+            "message" => "Fetched all maps",
+            "errorCode" => null,
+            "data" => $data["map"]
+        ]);
+    }
+    public function view($id){
+        $data["map"] = Map::findOrFail($id);
+        return response()->json([
+            "error" => false,
+            "message" => "Fetched map details.",
+            "errorCode" => null,
+            "data" => $data["map"]
+        ]);
+    }
+    public function update(Request $request, $id){
+        $data["map"] = Map::findOrFail($id);
+        $data["map"]->update($request->all());
+        return response()->json([
+            "error" => false,
+            "message" => "Update Successfull",
+            "errorCode" => null,
+            "data" => $data["map"]
+        ]);
+    }
+}
--- geodashboard-app/routes/web.php
+++ geodashboard-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\MapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,6 @@ Route::get('/layers', function () {
     return response()->json(\App\Models\Layer::all());
 });
 
-Route::get('/maps', function () {
-    return response()->json(\App\Models\Map::all());
-});
+Route::get('/maps', [MapController::class, 'index']);
+Route::get('/maps/{id}', [MapController::class, 'view']);
+Route::post('/maps/{id}/update', [MapController::class, 'update']);
