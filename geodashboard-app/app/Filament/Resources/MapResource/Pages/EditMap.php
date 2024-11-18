79df95be11acc20119437c246634e0ca061c947f
Subhadip G <subhadipghorui105@gmail.com>
2024-11-18 14:32:36 +0530

    update map api with join layer tabel

--- geodashboard-app/app/Filament/Resources/MapResource/Pages/EditMap.php
+++ geodashboard-app/app/Filament/Resources/MapResource/Pages/EditMap.php
@@ -21,18 +21,19 @@ class EditMap extends EditRecord
     protected function mutateFormDataBeforeSave(array $data): array
     {
 
-        $data['g_layers'] = array_map(function($item){
-            if(!empty($item['layers'])){
-                $item['layers'] = array_map(function($layer){
-                    $res = [];
-                    $res = Layer::findOrFail($layer["id"])->toArray();
-                    $res["checked"] = $layer["checked"];
-                    $res["status"] = $layer["status"];
-                    return $res;
-                }, $item['layers']);
-            }
-            return $item;
-        }, $data['g_layers']);
+        // $data['g_layers'] = array_map(function($item){
+        //     if(!empty($item['layers'])){
+        //         $item['layers'] = array_map(function($layer){
+        //             $res = [];
+        //             $res = Layer::findOrFail($layer["id"])->toArray();
+        //             $res["checked"] = $layer["checked"];
+        //             $res["status"] = $layer["status"];
+        //             return $res;
+        //         }, $item['layers']);
+        //     }
+        //     return $item;
+        // }, $data['g_layers']);
+        
         $data['g_meta'] = gettype($data['g_meta']) == 'array' ? $data['g_meta'] : json_decode($data['g_meta']);
         return $data;
     }
--- geodashboard-app/app/Http/Controllers/Api/v1/MapController.php
+++ geodashboard-app/app/Http/Controllers/Api/v1/MapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Layer;
 use App\Models\Map;
 use Illuminate\Http\Request;
 
@@ -19,6 +20,18 @@ class MapController extends Controller
     }
     public function view($id){
         $data["map"] = Map::where('g_uuid', $id)->first();
+        $data["map"]->g_layers = array_map(function($item){
+            if(!empty($item['layers'])){
+                $item['layers'] = array_map(function($layer){
+                    $res = [];
+                    $res = Layer::findOrFail($layer["id"])->toArray();
+                    $res["checked"] = $layer["checked"];
+                    $res["status"] = $layer["status"];
+                    return $res;
+                }, $item['layers']);
+            }
+            return $item;
+        }, $data["map"]->g_layers);
         return response()->json([
             "error" => false,
             "message" => "Fetched map details.",
