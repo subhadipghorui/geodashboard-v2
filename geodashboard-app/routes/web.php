66fb71352c4017f6af1a851f41c4a96e8ef7ab8c
Subhadip G <subhadipghorui105@gmail.com>
2024-11-19 16:01:32 +0530

    mvt and proxy api are auth gurded

--- geodashboard-app/routes/web.php
+++ geodashboard-app/routes/web.php
@@ -19,6 +19,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::group(["middleware" => ["auth", "user"]], function($routes){
     Route::get('/dashboard/', [DashboardController::class, 'index'])->name('app.dashboard.index');
+
+    Route::get('/mvt', [MVTController::class, 'mvt'])->middleware('throttle:1000,1');
+    Route::any('/tomcat-proxy{any?}', [ReverseProxyController::class, 'tomcatProxy'])->middleware('throttle:1000,1')->where('any', '.*');
 });
 Route::get('/dashboard/{id}', [DashboardController::class, 'view'])->name('app.dashboard.view');
 
@@ -26,6 +29,4 @@ Route::get('/maps', [MapController::class, 'index']);
 Route::get('/maps/{id}', [MapController::class, 'view']);
 Route::post('/maps/{id}/update', [MapController::class, 'update']);
 
-Route::get('/mvt', [MVTController::class, 'mvt'])->middleware('throttle:1000,1');
-Route::any('/tomcat-proxy{any?}', [ReverseProxyController::class, 'tomcatProxy'])->middleware('throttle:1000,1')->where('any', '.*');
 
